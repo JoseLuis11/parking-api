c1855dbf02d1ca5a44911ab813ba560729dbc0e0
Jose Luis Castro <jluis.scooter11@hotmail.com>
2023-03-25 19:53:43 -0600

    Feat: adds createdAt, and updatedAt columns to parking_lots table

--- sequelize/migrations/202303251337-create-parking-lots.cjs
+++ sequelize/migrations/202303251337-create-parking-lots.cjs
@@ -1,29 +1,37 @@
 module.exports = {
   up: (queryInterface, DataTypes) => {
     return queryInterface.createTable('parking_lots', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER
-      },
-      name: {
-        allowNull: false,
-        type: DataTypes.STRING,
-        unique: true
-      },
-      spots: {
-        allowNull: false,
-        type: DataTypes.INTEGER
-      },
-      contact: {
-        allowNull: true,
-        type: DataTypes.STRING
-      },
-      parkingType: {
-        allowNull: false,
-        type: DataTypes.STRING
-      }
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER
+        },
+        name: {
+          allowNull: false,
+          type: DataTypes.STRING,
+          unique: true
+        },
+        spots: {
+          allowNull: false,
+          type: DataTypes.INTEGER
+        },
+        contact: {
+          allowNull: true,
+          type: DataTypes.STRING
+        },
+        parkingType: {
+          allowNull: false,
+          type: DataTypes.STRING
+        },
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        }
       },
       {
         charset: 'utf8'
