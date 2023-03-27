5c0b840142d281b276ca04d6ff19c698cd02d886
Jose Luis Castro <jluis.scooter11@hotmail.com>
2023-03-27 00:44:31 -0600

    Docs: replaces all database env variables into a DB_URL env variable

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -3,10 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  'username': process.env.DB_USER,
-  'password': process.env.DB_PASSWORD,
-  'database': process.env.DB_DATABASE,
-  'host': process.env.DB_HOST,
-  'port': process.env.DB_PORT,
-  'dialect': 'postgres'
+  'url': process.env.DB_URL,
+  'dialect': 'postgres',
+  'seederStorage': 'sequelize'
 }
